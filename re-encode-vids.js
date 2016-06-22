77ff0f9cf0f1eb49d7ec72fa52989c8a5b995ba9
Christian Parker <christian.parker@emc.com>
2016-06-22 16:05:38 -0500

    better

--- re-encode-vids.js
+++ re-encode-vids.js
@@ -59,99 +59,100 @@ module.exports = (function() {
 
     var vids = fs.readdirSync(newAVIsDir)
     console.log('', vids.length, 'new videos to re-encode');
+    
+    var encodeCommand = '/app/vendor/libav/bin/avconv -y -i $INPUT -vcodec libx264 -vprofile high -preset slow -b:v 100k -maxrate 100k -bufsize 200k -r 4 $OUTPUT';
+    var animatedGifCommand = "/app/ffmpeg -t 20 -i $INPUT -r 1 -vf 'select=gt(scene\\,0.1),scale=350:-1' -gifflags +transdiff -y $OUTPUT";
+    var simpleOneFrameCommand = "/app/ffmpeg -y -i $INPUT -vframes 1 -vf 'scale=350:-1' $OUTPUT";
+
+    var logExecIO = function(io) {
+      console.log('STDIN: ', io[0]);
+      console.log('STDERR: ', io[1]);
+    };
+
+    var logExecErr = function(err) {
+      console.log('error during exec', err);
+    };
+
+    var commandPromises = _.flatten(_.map(vids, function(vid) {
+      console.log('working on ', vid)
+
+      var newVidMp4Name = vid.replace(/^(.*?)\.avi$/, '$1.mp4');
+      var newVidGifName = vid.replace(/^(.*?)\.avi$/, '$1.gif');
+      console.log('newVidMp4Name ', newVidMp4Name, ' newVidGifName ', newVidGifName)
+
+      var reEncodeCommand = encodeCommand
+        .replace('$INPUT', `${newAVIsDir}/${vid}`)
+        .replace('$OUTPUT', `${encodedMP4sDir}/${newVidMp4Name}`)
+
+      console.log('reEncodeCommand', reEncodeCommand)
+
+      var aniGifFinalCommand = animatedGifCommand
+        .replace('$INPUT', `${encodedMP4sDir}/${newVidMp4Name}`)
+        .replace('$OUTPUT', `${stillPicDir}/${newVidGifName}`)
+
+      console.log('aniGifFinalCommand', aniGifFinalCommand)
+
+      var simpleOneFrameFinalCommand = simpleOneFrameCommand
+        .replace('$INPUT', `${encodedMP4sDir}/${newVidMp4Name}`)
+        .replace('$OUTPUT', `${stillPicDir}/${newVidGifName}`)
+
+      console.log('simpleOneFrameCommand', simpleOneFrameCommand)
+
+      var reEncodeFunc = function() {
+        console.log('running 1.', reEncodeCommand);
+        return execPromise(reEncodeCommand)
+          .then(logExecIO)
+          .then(function() {
+            console.log('deleting source file')
+            unlink(`${encodedMP4sDir}/${vid}`)
+          })
+          .catch(logExecErr)
+          .then(function() {
+            console.log('deleting source file');
+            unlink(`${encodedMP4sDir}/${vid}`)
+          })
+      };
+
+      var gifFailRX = new RegExp('^.*?(Output file is empty).*$', 'mi');
+
+      var simpleOneFrameFunc = function() {
+        console.log('running 3.', simpleOneFrameCommand);
+
+        return execPromise(simpleOneFrameFinalCommand)
+          .then(logExecIO)
+          .catch(logExecErr);
+      };
+
+      var makeGifFunc = function() {
+        console.log('running 2.', aniGifFinalCommand);
+        return execPromise(aniGifFinalCommand)
+          .then(function(res) {
+            console.log(res[0]);
+            console.log(res[1]);
+
+            // when the ani gif step fails (it does sometimes depending on the content), it logs:
+            //
+            // 'Output file is empty, nothing was encoded (check -ss / -t / -frames parameters if used)'
+            //
+            // if that happens, fall back to a simpler method that captures one frame from the vid
+            if (gifFailRX.exec(res[0] + res[1]) != null) {
+              console.log('Creating animated gif failed, so falling back to simple one-frame .gif');
+              return simpleOneFrameFunc();
+            }
+          })
+          .catch(logExecErr);
+      };
+
+      // returning an array, which gets flattened
+      return [reEncodeFunc, makeGifFunc];
+
+    }));
+
+    // now execute the promises sequentially
+    commandPromises.reduce(Q.when, Q('initial'));
+
   })
 
-
-  var encodeCommand = '/app/vendor/libav/bin/avconv -y -i $INPUT -vcodec libx264 -vprofile high -preset slow -b:v 100k -maxrate 100k -bufsize 200k -r 4 $OUTPUT';
-  var animatedGifCommand = "/app/ffmpeg -t 20 -i $INPUT -r 1 -vf 'select=gt(scene\\,0.1),scale=350:-1' -gifflags +transdiff -y $OUTPUT";
-  var simpleOneFrameCommand = "/app/ffmpeg -y -i $INPUT -vframes 1 -vf 'scale=350:-1' $OUTPUT";
-
-  var logExecIO = function(io) {
-    console.log('STDIN: ', io[0]);
-    console.log('STDERR: ', io[1]);
-  };
-
-  var logExecErr = function(err) {
-    console.log('error during exec', err);
-  };
-
-  var commandPromises = _.flatten(_.map(vids, function(vid) {
-    console.log('working on ', vid)
-
-    var newVidMp4Name = vid.replace(/^(.*?)\.avi$/, '$1.mp4');
-    var newVidGifName = vid.replace(/^(.*?)\.avi$/, '$1.gif');
-    console.log('newVidMp4Name ', newVidMp4Name, ' newVidGifName ', newVidGifName)
-
-    var reEncodeCommand = encodeCommand
-      .replace('$INPUT', `${newAVIsDir}/${vid}`)
-      .replace('$OUTPUT', `${encodedMP4sDir}/${newVidMp4Name}`)
-
-    console.log('reEncodeCommand', reEncodeCommand)
-
-    var aniGifFinalCommand = animatedGifCommand
-      .replace('$INPUT', `${encodedMP4sDir}/${newVidMp4Name}`)
-      .replace('$OUTPUT', `${stillPicDir}/${newVidGifName}`)
-
-    console.log('aniGifFinalCommand', aniGifFinalCommand)
-
-    var simpleOneFrameFinalCommand = simpleOneFrameCommand
-      .replace('$INPUT', `${encodedMP4sDir}/${newVidMp4Name}`)
-      .replace('$OUTPUT', `${stillPicDir}/${newVidGifName}`)
-
-    console.log('simpleOneFrameCommand', simpleOneFrameCommand)
-
-    var reEncodeFunc = function() {
-      console.log('running 1.', reEncodeCommand);
-      return execPromise(reEncodeCommand)
-        .then(logExecIO)
-        .then(function() {
-          console.log('deleting source file')
-          unlink(`${encodedMP4sDir}/${vid}`)
-        })
-        .catch(logExecErr)
-        .then(function() {
-          console.log('deleting source file');
-          unlink(`${encodedMP4sDir}/${vid}`)
-        })
-    };
-
-    var gifFailRX = new RegExp('^.*?(Output file is empty).*$', 'mi');
-
-    var simpleOneFrameFunc = function() {
-      console.log('running 3.', simpleOneFrameCommand);
-
-      return execPromise(simpleOneFrameFinalCommand)
-        .then(logExecIO)
-        .catch(logExecErr);
-    };
-
-    var makeGifFunc = function() {
-      console.log('running 2.', aniGifFinalCommand);
-      return execPromise(aniGifFinalCommand)
-        .then(function(res) {
-          console.log(res[0]);
-          console.log(res[1]);
-
-          // when the ani gif step fails (it does sometimes depending on the content), it logs:
-          //
-          // 'Output file is empty, nothing was encoded (check -ss / -t / -frames parameters if used)'
-          //
-          // if that happens, fall back to a simpler method that captures one frame from the vid
-          if (gifFailRX.exec(res[0] + res[1]) != null) {
-            console.log('Creating animated gif failed, so falling back to simple one-frame .gif');
-            return simpleOneFrameFunc();
-          }
-        })
-        .catch(logExecErr);
-    };
-
-    // returning an array, which gets flattened
-    return [reEncodeFunc, makeGifFunc];
-
-  }));
-
-  // now execute the promises sequentially
-  commandPromises.reduce(Q.when, Q('initial'));
 
 
 
