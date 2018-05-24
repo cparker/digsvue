2d19da03823fa5c3bc0c175804eb57bafe3b5083
Christian Parker <christian.parker@emc.com>
2018-05-24 10:19:10 -0600

    added state lambda function

---
+++ digsvueStateLambda.js
@@ -0,0 +1,63 @@
+const AWS = require("aws-sdk");
+let docClient = new AWS.DynamoDB.DocumentClient();
+
+/**
+ * for handling the state of the digsvue camera linux motion ras pi
+ * 
+ */
+exports.handler = (event, context, callback) => {
+
+    console.log('incoming event', event);
+
+    const genericResponse = {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        isBase64Encoded: false,
+    }
+
+    if (event.httpMethod === 'GET') {
+        const dbParams = {
+            TableName: 'digsvueState',
+            Key: { id: '1' }
+        }
+
+        docClient.get(dbParams, (err, data) => {
+            if (err) {
+                genericResponse.body = JSON.stringify(err)
+                callback(null, genericResponse)
+            } else {
+                genericResponse.body = JSON.stringify(data.Item)
+                callback(null, genericResponse)
+            }
+        })
+
+    } else if (event.httpMethod === 'POST') {
+        const toInsert = {}
+        const parsedBody = JSON.parse(event.body)
+        Object.assign(toInsert, parsedBody)
+        toInsert.id = '1'
+        
+        const dbParams = {
+            TableName: 'digsvueState',
+            Item: toInsert
+        }
+        console.log('inserting', dbParams)
+
+        docClient.put(dbParams, (err, data) => {
+            if (err) {
+                genericResponse.body = JSON.stringify(err)
+                callback(null, genericResponse)
+            } else {
+                genericResponse.body = JSON.stringify(data)
+                callback(null, genericResponse)
+            }
+        })
+
+    } else {
+        genericResponse.body = JSON.stringify({ message: `no handler for ${event.httpMethod}` })
+        callback(null, genericResponse)
+    }
+
+};
